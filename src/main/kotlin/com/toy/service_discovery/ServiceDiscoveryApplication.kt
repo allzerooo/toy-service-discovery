1295c828b6e7e68a19f3fbd2812355f5bea58dff
allzerooo <dychoi.dev@gmail.com>
2025-10-31 17:46:57 +0900

    프로젝트 생성

---
+++ src/main/kotlin/com/toy/service_discovery/ServiceDiscoveryApplication.kt
@@ -0,0 +1,11 @@
+package com.toy.service_discovery
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ServiceDiscoveryApplication
+
+fun main(args: Array<String>) {
+	runApplication<ServiceDiscoveryApplication>(*args)
+}
---
+++ src/test/kotlin/com/toy/service_discovery/ServiceDiscoveryApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.toy.service_discovery
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ServiceDiscoveryApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
